cf65546c6d3269f896d5fccc26f7ee95ce7fd280
Okami <omarzerpa316@gmail.com>
2023-05-19 19:27:56 -0300

    mi primer commit

---
+++ ejercicioJuilacion.js
@@ -0,0 +1,17 @@
+/* Escribí la función puedeJubilarse que recibe por parámetro la edad, el género y además,
+ los años de aportes jubilatorios (en ese orden) que posee una persona, ejemplo:
+ Tené en cuenta que el mínimo de edad para realizar el trámite para las mujeres (género "F") 
+es de 60 años, mientras que para los hombres (género "M") es 65. En ambos casos, se 
+deben contar con al menos 30 años de aportes jubilatorios.
+Te recomendamos que primero trates de resolver el ejercicio, después fijate lo que sigue y s
+i podes resolverlo de ésta otra manera, deberíamos usar funciones 
+dentro de otra función, eso ya lo hicimos en funcionception , ¿Lo recordás? */
+
+function puedeJubilarse(edad, genero, aniosAporte) {
+    
+ if (edad => 60) {
+    
+ }
+}
+
+genero === "F" || "M") && tieneSuficientesAportes(aniosAporte >= 30
